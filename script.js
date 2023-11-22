9a45937a6264922ca903d230341ec46f6bce8077
Rayyan013 <rayyanahmed927@gmail.com>
2023-11-22 20:15:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,178 @@
+gsap.to("#ball1", {
+    y:-500,
+    duration:2,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "red"
+})
+
+gsap.to("#ball2", {
+    y:-500,
+    duration:2,
+    delay:1,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "yellow"
+})
+
+gsap.to("#ball3", {
+    y:-500,
+    duration:2,
+    delay:2,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "green"
+})
+
+gsap.to("#ball4", {
+    y:-500,
+    duration:2,
+    delay:3,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "salmon"
+})
+
+gsap.to("#ball5", {
+    y:-500,
+    duration:2,
+    delay:4,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "skyblue"
+})
+
+gsap.to("#ball6", {
+    y:-500,
+    duration:2,
+    delay:5,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "yellow"
+})
+
+gsap.to("#ball7", {
+    y:-500,
+    duration:2,
+    delay:6,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "red"
+})
+
+gsap.to("#ball8", {
+    y:-500,
+    duration:2,
+    delay:7,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "yellow"
+})
+
+gsap.to("#ball9", {
+    y:-500,
+    duration:2,
+    delay:8,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "aqua"
+})
+
+gsap.to("#ball10", {
+    y:-500,
+    duration:2,
+    delay:9,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "chocolate"
+})
+
+gsap.to("#ball11", {
+    y:-500,
+    duration:2,
+    delay:10,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "brown"
+})
+
+gsap.to("#ball12", {
+    y:-500,
+    duration:2,
+    delay:11,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "blueviolet"
+})
+
+gsap.to("#ball13", {
+    y:-500,
+    duration:2,
+    delay:12,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "yellow"
+})
+
+gsap.to("#ball14", {
+    y:-500,
+    duration:2,
+    delay:13,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "red"
+})
+
+gsap.to("#ball15", {
+    y:-500,
+    duration:2,
+    delay:14,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "green"
+})
+
+gsap.to("#ball16", {
+    y:-500,
+    duration:2,
+    delay:15,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "salmon"
+})
+
+gsap.to("#ball17", {
+    y:-500,
+    duration:2,
+    delay:16,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "cadetblue"
+})
+
+gsap.to("#ball18", {
+    y:-500,
+    duration:2,
+    delay:17,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "burlywood"
+})
+
+gsap.to("#ball19", {
+    y:-500,
+    duration:2,
+    delay:18,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "yellow"
+})
+
+gsap.to("#ball20", {
+    y:-500,
+    duration:2,
+    delay:19,
+    repeat: -1,
+    yoyo: "true",
+    backgroundColor: "skyblue"
+})
